442b1f4709ce60a3763526a231bb2714c6a6b9f3
Monika <misswysocki@gmail.com>
2015-09-20 13:00:32 -0400

    admin exercize

--- admin.rb
+++ admin.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
-#you run a site that offers a matching service between vendors selling laptops and customers 
-#..looking to purchase; you charge a fee to be listed as a vendor 
+#you run a site that offers a matching service between vendors selling laptops and customers
+#..looking to purchase; you charge a fee to be listed as a vendor
 #your site's users must sign in and if they are an admin they are able to make changes to the site
 
 #______________________________________________
@@ -11,22 +11,40 @@ require 'pry'
 #if the user is signed in and they are an admin, show "you can see see and change all the pages!"
 #if you user is signed in but they are not an admin, show "you can see all the pages!"
 #if the user is not signed in, show "you can't see any of the pages, please sign in!"
+def user_permission(signed_in, admin, paid, cancelled)
+    if paid == "no" or cancelled == "yes"
+      puts "go away!"
+    elsif signed_in == "yes" and admin == "yes"
+      puts "you can see see and change all the pages!"
+    elsif signed_in == "yes" and admin == "no"
+      puts "you can see all the pages!"
+    elsif signed_in == "no"
+      puts "you can't see any of the pages, please sign in!"
+    end
+end
 
 #ask the user if they pay their bills (yes/no)
+puts "Do you pay your bills? yes or no"
 
 #store their answer in a variable
+user_paid = gets.strip
 
 #ask the user if they have canceled a deal (yes/no)
+puts "Have you canceled a deal? yes or no"
 
 #store their answer in a variable
+user_cancelled = gets.strip
 
 #ask the user if they are an admin (yes/no)
+puts "Are you an admin? yes or no"
 
 #store their answer in a variable
+user_admin = gets.strip
 
 #ask the user if they are signed in (yes/no)
+puts "Are you signed in? yes or no"
 
 #store their answer in a variable
+user_signed_in = gets.strip
 
-
-
+user_permission(user_signed_in, user_admin, user_paid, user_cancelled)
