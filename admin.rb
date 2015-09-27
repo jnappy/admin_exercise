1f52990be67a11c5fa211bbccfb9d4543142afde
marcela villa <a.marcela.v@gmail.com>
2015-09-27 15:43:19 -0400

    adminex

--- admin.rb
+++ admin.rb
@@ -1,4 +1,73 @@
 require 'pry'
+ def user_permission(signed_in, admin, paid, canceled)
+   if paid == false && canceled == true
+     puts "Go Away!"
+   elsif signed_in == true && admin == true
+     puts "you can see and change all the pages!"
+   else signed_in == true && admin == false
+     puts "you can see all the pages!"
+     default
+     puts "you can't see any of the pages, please sign in"
+   end
+ end
+ puts "Did you pay your bill?"
+ bill_pay = gets.strip
+ puts "Have you canceled a dea?"
+ cancel = gets.strip
+ puts "Are you an admin?"
+ is_admin = gets.strip
+ puts "Are you signed in?"
+ signedin = gets.strip
+
+user_permission(bill_pay, cancel, is_admin, signedin)     
+
+#you run a site that offers a matching service between vendors selling laptops and customers
+#..looking to purchase; you charge a fee to be listed as a vendor
+#your site's users must sign in and if they are an admin they are able to make changes to the site
+
+#______________________________________________
+
+#write a function "user_permission" that accepts four parameters: signed_in, admin, paid, and canceled
+#if the user doesn't pay their bills or has canceled a deal, show "go away!" DONE
+
+#if the user is signed in and they are an admin, show "you can see see and change all the pages!"
+#if you user is signed in but they are not an admin, show "you can see all the pages!"
+#if the user is not signed in, show "you can't see any of the pages, please sign in!"
+
+#ask the user if they pay their bills (yes/no)
+
+#store their answer in a variable
+
+#ask the user if they have canceled a deal (yes/no)
+
+#store their answer in a variable
+
+#ask the user if they are an admin (yes/no)
+
+#store their answer in a variable
+
+#ask the user if they are signed in (yes/no)
+
+#store their answer in a variable
+
+
+#you run a site that offers a matching service between vendors selling laptops and customers
+#..looking to purchase; you charge a fee to be listed as a vendor
+#your site's users must sign in and if they are an admin they are able to make changes to the site
+
+#______________________________________________
+
+#write a function "user_permission" that accepts four parameters: signed_in, admin, paid, and canceled
+#if the user doesn't pay their bills or has canceled a deal, show "go away!"
+#if the user is signed in and they are an admin, show "you can see see and change all the pages!"
+#if you user is signed in but they are not an admin, show "you can see all the pages!"
+#if the user is not signed in, show "you can't see any of the pages, please sign in!"
+
+#ask the user if they pay their bills (yes/no)
+
+#store their answer in a variable
+
+#ask the user if they have canceled a deal (yes/no)
 
 #you run a site that offers a matching service between vendors selling laptops and customers
 #..looking to purchase; you charge a fee to be listed as a vendor
