9bbb897441f559942237424e0a4f1813e6e5168c
Hannah Bock <Hannah@Macintosh.local>
2015-09-27 16:05:13 -0400

    AGAIN.

--- admin.rb
+++ admin.rb
@@ -7,36 +7,41 @@ require 'pry'
 #______________________________________________
 
 #write a function "user_permission" that accepts four parameters: signed_in, admin, paid, and canceled
-def user_permission(user_bills, user_deal, user_admin, user_signin)
-#if the user doesn't pay their bills or has canceled a deal, show "go away!"
-  if user_bills.downcase == "no" or user_deal.downcase == "yes"
+def user_permission(paid, signed_in, canceled, admin)
+  if paid == "no"
     puts "go away!"
-#if the user is signed in and they are an admin, show "you can see see and change all the pages!"
-  elsif user_admin.downcase == "yes" and user_signin.downcase == "yes"
-    puts "you can see and change all the pages!"
-#if you user is signed in but they are not an admin, show "you can see all the pages!"
-  elsif user_admin.downcase == "no" and user_signin.downcase == "yes"
-    puts "you can see all the pages!"
-#if the user is not signed in, show "you can't see any of the pages, please sign in!"
-  elsif user_signin.downcase == "no"
-    puts "you can't see any of the pages, please sign in!"
+  elsif signed_in == "yes" and admin == "yes"
+      puts "you can see and change all the pages"
+    elsif signed_in == "yes" and admin == "no"
+      puts "you can see all the pages"
+    else signed_in == "no"
+      puts "you can't see any of thes pages, please sign in"
+    end
   end
-end
+
+
+#if the user doesn't pay their bills or has canceled a deal, show "go away!"
+#if the user is signed in and they are an admin, show "you can see see and change all the pages!"
+#if you user is signed in but they are not an admin, show "you can see all the pages!"
+#if the user is not signed in, show "you can't see any of the pages, please sign in!"
+
 #ask the user if they pay their bills (yes/no)
-puts "Do you pay your bills?"
+puts "Have you paid your bills?"
 #store their answer in a variable
-user_bills = gets.strip
+user_paid = gets.strip
+
 #ask the user if they have canceled a deal (yes/no)
-puts "Have you canceled a deal?"
+puts "have you canceled a deal"
 #store their answer in a variable
-user_deal = gets.strip
+user_canceled = gets.strip
+
 #ask the user if they are an admin (yes/no)
 puts "Are you an admin?"
 #store their answer in a variable
-user_admin = gets.strip
+user_admin = gets. strip
 #ask the user if they are signed in (yes/no)
-puts "Are you signed in?"
+puts "are you signed in?"
 #store their answer in a variable
-user_signin = gets.strip
+user_signed_in = gets.strip
 
-user_permission(user_bills, user_deal, user_admin, user_signin)
+user_permission(user_paid, user_signed_in, user_canceled, user_admin)
