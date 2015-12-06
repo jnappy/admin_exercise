c0bcb04cfa2aac3ff20a402b0106dcd1af70a8da
Kathy <katherine.a.cuevas@gmail.com>
2015-12-06 15:26:02 -0500

    admin_exercise

--- admin.rb
+++ admin.rb
@@ -1,30 +1,21 @@
-require 'pry'
+def user_permission
+  puts "Do you pay your bills? Please state yes or no"
+  bill_pay = gets.strip
+  puts "Have you cancelled a deal? Please state yes or no"
+  canceled_deal = gets.strip
+  puts "Are you an admin? Please state yes or no"
+  admin = gets.strip
+  puts "Are you signed in? Please state yes or no"
+  signed_in = gets.strip
+  if bill_pay == "no" or canceled_deal == "yes"
+    puts "Go away!"
+  elsif signed_in == "yes" and admin == "yes"
+    puts "You can see and change all the pages!"
+  elsif signed_in == "yes" and admin == "no"
+  puts "You can see all the pages."
+elsif signed_in == "no"
+  puts "You can't see any of the pages, please sign in."
+end
+end
 
-#you run a site that offers a matching service between vendors selling laptops and customers
-#..looking to purchase; you charge a fee to be listed as a vendor
-#your site's users must sign in and if they are an admin they are able to make changes to the site
-
-########################################################
-
-#write a function "user_permission" that accepts four parameters: signed_in, admin, paid, and canceled
-
-#ask the user if they pay their bills (yes/no)
-#store their answer in a variable
-
-#ask the user if they have canceled a deal (yes/no)
-#store their answer in a variable
-
-#ask the user if they are an admin (yes/no)
-#store their answer in a variable
-
-#ask the user if they are signed in (yes/no)
-#store their answer in a variable
-
-#if the user doesn't pay their bills or has canceled a deal, show "go away!"
-#if the user is signed in and they are an admin, show "you can see see and change all the pages!"
-#if you user is signed in but they are not an admin, show "you can see all the pages!"
-#if the user is not signed in, show "you can't see any of the pages, please sign in!"
-
-
-
-# call the function
+user_permission
