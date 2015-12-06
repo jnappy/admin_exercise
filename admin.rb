2bedc96333218152004a6ab04f8dd999faee25eb
Dwayne Sturgeon <phillip.lamplugh+dwaynesturgeon@gmail.com>
2015-12-06 15:23:52 -0500

    updated logic for access

--- admin.rb
+++ admin.rb
@@ -1,30 +1,38 @@
 require 'pry'
 
-#you run a site that offers a matching service between vendors selling laptops and customers
-#..looking to purchase; you charge a fee to be listed as a vendor
-#your site's users must sign in and if they are an admin they are able to make changes to the site
+def user_permission
 
-########################################################
+puts "did you pay your bills? (yes/no)"
+paid = gets.strip
 
-#write a function "user_permission" that accepts four parameters: signed_in, admin, paid, and canceled
+puts "did you cancel"
+canceled = gets.strip
 
-#ask the user if they pay their bills (yes/no)
-#store their answer in a variable
+puts "are you an admin"
+admin = gets.strip
 
-#ask the user if they have canceled a deal (yes/no)
-#store their answer in a variable
+puts "are you signed in"
+signed_in = gets.strip
 
-#ask the user if they are an admin (yes/no)
-#store their answer in a variable
 
-#ask the user if they are signed in (yes/no)
-#store their answer in a variable
+if paid == "no" or canceled == "yes"
+  puts "go away"
 
-#if the user doesn't pay their bills or has canceled a deal, show "go away!"
-#if the user is signed in and they are an admin, show "you can see see and change all the pages!"
-#if you user is signed in but they are not an admin, show "you can see all the pages!"
-#if the user is not signed in, show "you can't see any of the pages, please sign in!"
 
+elsif signed_in == "yes" and admin == "yes"
+  puts "you can see and change all the pages"
+
+
+elsif signed_in == "yes" and admin == "no"
+  puts "you can see all the pages"
+
+elsif signed_in == "no"
+  puts "you can't see any of the pages, please sign in"
+else
+  "sorry charlie"
+end
+end
+user_permission
 
 
 # call the function
